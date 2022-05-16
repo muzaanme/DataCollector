ddba37006c0e6afd37d2625e674fe1833c2fb85f
muzaanme <muzaanme@gmail.com>
2022-05-16 19:23:01 +0530

    added table and name field in database

--- app.py
+++ app.py
@@ -14,12 +14,15 @@ db.init_app(app)
 class Data(db.Model):
     __tablename__ = "data"
     id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20), nullable = False)
     email_ = db.Column(db.String(120), unique = True, nullable  = False)
     height_ = db.Column(db.Integer, nullable = False)
 
-    def __init__(self, email_, height_):
+    def __init__(self, name, email_, height_):
+        self.name = name
         self.email_ = email_
         self.height_ = height_
+        
 
     
 
@@ -36,20 +39,24 @@ def Home():
 @app.route("/success", methods =["POST"])
 def successPage():
     if request.method=="POST":
+        name_ = request.form["user_name"]
         email = request.form["email_name"]
         height = request.form["height_name"]
+        
 #send_email(email, height)
         if db.session.query(Data).filter(Data.email_== email).count() == 0:
-            data = Data(email, height)
+            data = Data(name_, email, height)
             db.session.add(data)
             db.session.commit()
             average_height = db.session.query(func.avg(Data.height_)).scalar()
             average_height = round(average_height, 1)
-            #view_all = db.session.query(Data.email_).all()
-            # for entries in view_all:
-            #     print(entries)
-            print(average_height)
-            return render_template("success.html", heights=height, avg = average_height)
+            userName =  db.session.query(Data.name).all()
+            mail = db.session.query(Data.email_).all()
+            heit = db.session.query(Data.height_).all()
+            # for i in heit:
+            #     print(i)
+            # print(average_height)
+            return render_template("success.html", heights=height, avg = average_height,mail= mail,heit= heit, userName = userName)
         return render_template("index.html",
         text = "Seems like we've got something from the Email Address already!")
 
